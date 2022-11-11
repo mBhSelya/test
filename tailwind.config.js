327003d033baba0dcd2edf99a95529b71f92877a
Aleksey Selyakov <selyakoff18@gmail.com>
2022-11-11 17:20:12 +0300

    Исправление ошибок

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {},
+    screens: {
+      md: '680px',
+      lg: '1149px',
+      xl: '1248px',
+    }
   },
   plugins: [],
 }
